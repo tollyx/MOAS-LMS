95daf4b63fcf6deaa9288058b468e28a5beb85ec
Oscar Ljungdahl <oscar.ljungdahl@hotmail.com>
2018-07-18 15:04:04 +0200

    activityModel

---
+++ MOAS-LMS/Models/Activity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace MOAS_LMS.Models
+{
+    public class Activity
+    {
+        public string ActivityType { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+    }
+}
