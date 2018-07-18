95036d21786102d22446f84eeade74984ba404db
Adrian Hedqvist <adrian@tollyx.net>
2018-07-18 15:00:01 +0200

    Scaffold CourseController

--- MOAS-LMS/Controllers/AccountController.cs
+++ MOAS-LMS/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace MOAS_LMS.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Course");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace MOAS_LMS.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Course");
         }
 
         //
@@ -449,7 +449,7 @@ namespace MOAS_LMS.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Course");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
