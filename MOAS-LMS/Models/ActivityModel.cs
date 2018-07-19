29f3dd2e1a06f55445c8dd711c789a24449465b7
andersson257 <andersson257@gmail.com>
2018-07-19 16:05:46 +0200

    Created activitytype model, referenced it in activitymodel och updated seed

--- MOAS-LMS/Models/ActivityModel.cs
+++ MOAS-LMS/Models/ActivityModel.cs
@@ -6,7 +6,7 @@ namespace MOAS_LMS.Models
     public class ActivityModel
     {
         public int Id { get; set; }
-        public string ActivityType { get; set; }
+        public virtual ActivityType ActivityType { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
