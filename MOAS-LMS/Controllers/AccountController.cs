18d7cc6428048d34c26b7b0423bb4d44c8874e22
Adrian Hedqvist <adrian@tollyx.net>
2018-07-25 11:45:01 +0200

    Fix authorization for multiple things

--- MOAS-LMS/Controllers/AccountController.cs
+++ MOAS-LMS/Controllers/AccountController.cs
@@ -134,42 +134,6 @@ namespace MOAS_LMS.Controllers
             }
         }
 
-        //
-        // GET: /Account/Register  
-        [Authorize(Roles = "Admin")]
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/Register
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid) {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded) {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Course");
-                }
-                AddErrors(result);
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
         // GET: /Account/RegisterStudent 
         [Authorize(Roles = "Admin")]
         public ActionResult RegisterStudent(int id)
@@ -214,7 +178,7 @@ namespace MOAS_LMS.Controllers
         }
         //
         // GET: /Account/Register
-        [AllowAnonymous]
+        [Authorize(Roles = "Admin")]
         public ActionResult RegisterTeacher()
         {
             return View();
@@ -223,7 +187,7 @@ namespace MOAS_LMS.Controllers
         //
         // POST: /Account/Register
         [HttpPost]
-        [AllowAnonymous]
+        [Authorize(Roles = "Admin")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> RegisterTeacher(RegisterViewModel model)
         {
