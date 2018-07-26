1712308355c93680aa66f0f3582265dca30e9bb3
normansebastian <38253015+normansebastian@users.noreply.github.com>
2018-07-26 13:36:57 +0200

    Removed ActionResult "Register", since it isn't used

--- MOAS-LMS/Controllers/AccountController.cs
+++ MOAS-LMS/Controllers/AccountController.cs
@@ -20,8 +20,6 @@ namespace MOAS_LMS.Controllers
         private ApplicationUserManager _userManager;
         private ApplicationDbContext _db;
 
-        private ApplicationDbContext db = new ApplicationDbContext();
-
         public AccountController()
         {
         }
@@ -134,42 +132,6 @@ namespace MOAS_LMS.Controllers
                     ModelState.AddModelError("", "Invalid code.");
                     return View(model);
             }
-        }
-
-        //
-        // GET: /Account/Register  
-        [Authorize(Roles = "Admin")]
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/Register
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid) {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded) {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Course");
-                }
-                AddErrors(result);
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
         }
 
         // GET: /Account/RegisterStudent 
